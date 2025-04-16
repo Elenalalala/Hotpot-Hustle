8d7a78af16d001612b79395d365714bb640c6a7e
Larabbit <pengx091@newschool.edu>
2025-04-16 17:05:24 -0400

    food class

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+
+public enum FOOD_STATUS
+{
+    RAW,
+    UNDERCOOKED,
+    COOKED,
+    OVERCOOKED,
+}
+
+[RequireComponent(typeof(MeshCollider))]
+public class Food : MonoBehaviour
+{
+    public FOOD_STATUS status;
+
+    /** When cooking_time is between perfect_start and perfect_end, the food status is COOKED. **/
+    [Range(1, 10)]
+    public float perfect_start;
+    [Range(1, 10)]
+    public float perfrect_end;
+
+    public float stiffness;
+    public float slipperiness;
+
+    private float cooking_time;
+    private IEnumerator cor_cooking;
+
+    private MeshCollider collider;
+
+    void Start()
+    {
+        status = FOOD_STATUS.RAW;
+        cooking_time = 0.0f;
+        collider = GetComponent<MeshCollider>();
+    }
+
+    public void StartCooking()
+    {
+        cor_cooking = Cooking(perfrect_end);
+        StartCoroutine(cor_cooking);
+    }
+
+    public void StopCooking()
+    {
+        if (cor_cooking == null) {
+            Debug.Log("This food has not been cooked yet.");
+            return;
+        }
+        StopCoroutine(cor_cooking);
+        Debug.Log(this.gameObject.name + " stops cooking.");
+    }
+
+    private IEnumerator Cooking(float done_time)
+    {
+        Debug.Log(this.gameObject.name + " starts cooking !");
+        while (cooking_time < done_time)
+        {
+            cooking_time += Time.deltaTime;
+            yield return null;
+
+        }
+        Debug.Log(this.gameObject.name + " is burnt !");
+    }
+
+    /* Collision */
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("chopstick"))
+        {
+
+        }
+        if (collision.collider.CompareTag("water"))
+        {
+            StartCooking();
+        }
+    }
+
+    public bool CanHold(float strength)
+    {
+        return strength > slipperiness;
+    }
+
+    public bool WillBreak(float strength)
+    {
+        return strength > stiffness;
+    }
+
+}
