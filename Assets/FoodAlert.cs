6410e487503fa6ad253b9e2fea4bb8faa0d653f9
JiaoMaMa <ckneer@seas.upenn.edu>
2025-04-28 14:45:25 -0400

    Elena's food alert polish

--- Assets/FoodAlert.cs
+++ Assets/FoodAlert.cs
@@ -3,18 +3,28 @@ using UnityEngine;
 public class FoodAlert : MonoBehaviour
 {
     private Camera main_cam;
+    public float height;
+    private Food parent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         main_cam = Camera.main;
         this.gameObject.SetActive(false);
+        parent = this.gameObject.GetComponentInParent<Food>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (parent != null)
+        {
+            this.gameObject.SetActive(parent.status == FOOD_STATUS.COOKING);
+        }
         this.gameObject.transform.LookAt(main_cam.transform.position);
+        Vector3 forward = transform.forward;
+
+        this.gameObject.transform.position = new Vector3(transform.parent.position.x,  GameManager.Instance.waterManager.waterPlane.gameObject.transform.position.y + height, transform.parent.position.z);
     }
 
     public void Activate(bool isActive)
